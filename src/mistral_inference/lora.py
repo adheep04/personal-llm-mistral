19dc8597ad198e882787b9f7b00157b3d12c8b87
Adheep Rampure <adheep@gmail.com>
2025-01-25 17:32:27 -0500

    using hqq for quantization

--- src/mistral_inference/lora.py
+++ src/mistral_inference/lora.py
@@ -8,8 +8,6 @@ import torch
 import torch.nn as nn
 from simple_parsing.helpers import Serializable
 
-from bitsandbytes import nn as bnb
-
 
 @dataclass
 class LoraArgs(Serializable):
@@ -32,9 +30,6 @@ class LoRALinear(nn.Module):
           connection versus original frozen weight. General guidance is
           to keep it to 2.0 and sweep over learning rate when changing
           the rank.
-    
-    EDITS:
-        - adding compatability with bitsandbytes
     """
 
     def __init__(
@@ -43,8 +38,6 @@ class LoRALinear(nn.Module):
         out_features: int,
         rank: int,
         scaling: float,
-        compute_dtype: torch.dtype = torch.float16,  # <-- New arg
-        quant_type: str = "nf4",  # <-- New arg
         bias: bool = False,
     ):
         super().__init__()
@@ -66,15 +59,8 @@ class LoRALinear(nn.Module):
             self.out_features,
             bias=self.bias,
         )
-        
-        self.linear = bnb.Linear4bit(
-            input_features=self.in_features, 
-            output_features=self.out_features, 
-            bias=self.bias,
-            compute_dtype=compute_dtype,
-            quant_type=quant_type
-            )
-        self.linear.weight.requires_grad = False
+
+        self.linear = nn.Linear(self.in_features, self.out_features, bias=self.bias)
 
         # make sure no LoRA weights are marked as "missing" in load_state_dict
         def ignore_missing_keys(m: nn.Module, incompatible_keys: NamedTuple) -> None:
@@ -83,9 +69,9 @@ class LoRALinear(nn.Module):
         self.register_load_state_dict_post_hook(ignore_missing_keys)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        lora = self.lora_B(self.lora_A(x)) * self.scaling
-        base = self.linear(x)
-        return lora + base
+        lora = self.lora_B(self.lora_A(x))
+        result: torch.Tensor = self.linear(x) + lora * self.scaling
+        return result
 
     def _load_from_state_dict(self, state_dict: Dict[str, Any], prefix: str, *args, **kwargs) -> None:  # type: ignore[no-untyped-def]
         key_name = prefix + "weight"
--- src/mistral_inference/transformer.py
+++ src/mistral_inference/transformer.py
@@ -8,6 +8,7 @@ from typing import Any, List, Mapping, Optional, Union
 import safetensors.torch
 import torch
 from torch import nn
+import torch.distributed
 
 from mistral_inference.args import TransformerArgs
 from mistral_inference.cache import BufferCache, CacheInputMetadata
@@ -16,8 +17,6 @@ from mistral_inference.model import ModelBase
 from mistral_inference.rope import precompute_freqs_cis
 from mistral_inference.transformer_layers import RMSNorm, TransformerBlock
 from mistral_inference.vision_encoder import VisionLanguageAdapter, VisionTransformer
-
-from bitsandbytes.functional import quantize_4bit
 
 
 @dataclass
@@ -47,25 +46,21 @@ class Transformer(ModelBase, LoRALoaderMixin):
         self._precomputed_freqs_cis: Optional[torch.Tensor] = None
         assert self.vocab_size > 0
         assert pipeline_rank < num_pipeline_ranks, (pipeline_rank, num_pipeline_ranks)
-        self.pipeline_rank = pipeline_rank
-        self.num_pipeline_ranks = num_pipeline_ranks
         self.softmax_fp32 = softmax_fp32
 
         # Modules specific to some ranks:
         self.tok_embeddings: Optional[nn.Embedding] = None
         self.norm: Optional[RMSNorm] = None
         self.output: Optional[nn.Linear] = None
-        if pipeline_rank == 0:
-            self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
+        self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
 
-            self.vision_encoder: Optional[VisionTransformer] = None
-            self.vision_language_adapter: Optional[VisionLanguageAdapter] = None
-            if args.vision_encoder is not None:
-                self.vision_encoder = VisionTransformer(args.vision_encoder)
-                self.vision_language_adapter = VisionLanguageAdapter(args.vision_encoder.hidden_size, args.dim)
-        if pipeline_rank == num_pipeline_ranks - 1:
-            self.norm = RMSNorm(args.dim, eps=args.norm_eps)
-            self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
+        self.vision_encoder: Optional[VisionTransformer] = None
+        self.vision_language_adapter: Optional[VisionLanguageAdapter] = None
+        if args.vision_encoder is not None:
+            self.vision_encoder = VisionTransformer(args.vision_encoder)
+            self.vision_language_adapter = VisionLanguageAdapter(args.vision_encoder.hidden_size, args.dim)
+        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
         # Initialize all layers but slice off those not of this rank.
         layers = [
             TransformerBlock(
@@ -80,8 +75,8 @@ class Transformer(ModelBase, LoRALoaderMixin):
             )
             for _ in range(args.n_layers)
         ]
-        num_layers_per_rank = math.ceil(self.n_layers / self.num_pipeline_ranks)
-        offset = self.pipeline_rank * num_layers_per_rank
+        num_layers_per_rank = math.ceil(self.n_layers / 1)
+        offset = 0 * num_layers_per_rank
         end = min(self.n_layers, offset + num_layers_per_rank)
         self.layers = nn.ModuleDict({str(i): layers[i] for i in range(offset, end)})
         self.n_local_layers = len(self.layers)
@@ -162,15 +157,11 @@ class Transformer(ModelBase, LoRALoaderMixin):
         else:
             input_metadata = [SimpleInputMetadata.from_seqlens(seqlens, self.device) for _ in range(len(self.layers))]
 
-        if self.pipeline_rank == 0:
-            assert self.tok_embeddings is not None
-            if self.vision_encoder is not None and images:
-                h = self.embed_vision_language_features(input_ids, images)
-            else:
-                h = self.tok_embeddings(input_ids)
+        assert self.tok_embeddings is not None
+        if self.vision_encoder is not None and images:
+            h = self.embed_vision_language_features(input_ids, images)
         else:
-            h = torch.empty(num_toks, self.args.dim, device=self.device, dtype=self.dtype)
-            torch.distributed.recv(h, src=self.pipeline_rank - 1)
+            h = self.tok_embeddings(input_ids)
 
         # freqs_cis is always the same for every layer
         freqs_cis = self.freqs_cis[input_metadata[0].positions]
@@ -187,13 +178,11 @@ class Transformer(ModelBase, LoRALoaderMixin):
 
         if cache is not None:
             cache.update_seqlens(seqlens)
-        if self.pipeline_rank < self.num_pipeline_ranks - 1:
-            torch.distributed.send(h, dst=self.pipeline_rank + 1)
-            return h
-        else:
-            # Last rank has a final normalization step.
-            assert self.norm is not None
-            return self.norm(h)  # type: ignore
+
+        # Last rank has a final normalization step.
+        assert self.norm is not None
+        return self.norm(h)  # type: ignore
+
 
     def forward(
         self,
@@ -203,64 +192,35 @@ class Transformer(ModelBase, LoRALoaderMixin):
         images: Optional[List[torch.Tensor]] = None,
     ) -> torch.Tensor:
         h = self.forward_partial(input_ids, seqlens, cache=cache, images=images)
-        if self.pipeline_rank < self.num_pipeline_ranks - 1:
-            # ignore the intermediate activations as we'll get the final output from
-            # the last stage
-            outs = torch.empty(h.shape[0], self.vocab_size, device=h.device, dtype=h.dtype)
-        else:
-            assert self.output is not None
-            outs = self.output(h)
-        if self.num_pipeline_ranks > 1:
-            torch.distributed.broadcast(outs, src=self.num_pipeline_ranks - 1)
 
-        if self.softmax_fp32:
-            return outs.float()
-        else:
-            return outs
+        assert self.output is not None
+        outs = self.output(h)
+                                
+        return outs.float() if self.softmax_fp32 else outs
 
     def load_state_dict(self, state_dict: Mapping[str, Any], strict: bool = True, assign: bool = False) -> None:
         state_to_load = {}
         skipped = set([])
         for k, v in state_dict.items():
-            if k.startswith("tok_embeddings"):
-                if self.pipeline_rank == 0:
-                    state_to_load[k] = v
-                else:
-                    logging.debug(
-                        "Skipping parameter %s at pipeline rank %d",
-                        k,
-                        self.pipeline_rank,
-                    )
-                    skipped.add(k)
-            elif k.startswith("norm") or k.startswith("output"):
-                if self.pipeline_rank == self.num_pipeline_ranks - 1:
-                    state_to_load[k] = v
-                else:
-                    logging.debug(
-                        "Skipping parameter %s at pipeline rank %d",
-                        k,
-                        self.pipeline_rank,
-                    )
-                    skipped.add(k)
+            if (
+            k.startswith("tok_embeddings") or \
+            k.startswith("norm") or \
+            k.startswith("output") or \
+            k.startswith("vision_encoder") or \
+            k.startswith("vision_language_adapter")
+            ):
+                state_to_load[k] = v
             elif k.startswith("layers"):
                 layer_id = k.split(".")[1]
                 if layer_id in self.layers:
                     state_to_load[k] = v
                 else:
-                    logging.debug(
-                        "Skipping parameter %s at pipeline rank %d",
-                        k,
-                        self.pipeline_rank,
-                    )
                     skipped.add(k)
-            elif k.startswith("vision_encoder") or k.startswith("vision_language_adapter"):
-                assert not self.pipeline_rank
-                state_to_load[k] = v
             else:
                 raise ValueError(f"Unexpected key {k}")
         assert set(state_dict.keys()) == skipped.union(set(state_to_load.keys()))
         super().load_state_dict(state_to_load, strict=strict, assign=assign)
-
+        
     @staticmethod
     def from_folder(
         folder: Union[Path, str],
@@ -269,15 +229,14 @@ class Transformer(ModelBase, LoRALoaderMixin):
         device: Union[torch.device, str] = "cuda",
         dtype: Optional[torch.dtype] = None,
         softmax_fp32: bool = True,
-        quant_4bit: bool = True,
     ) -> "Transformer":
+        
         with open(Path(folder) / "params.json", "r") as f:
             model_args = TransformerArgs.from_dict(json.load(f))
         model_args.max_batch_size = max_batch_size
-        if num_pipeline_ranks > 1:
-            pipeline_rank = torch.distributed.get_rank()
-        else:
-            pipeline_rank = 0
+        
+        pipeline_rank = torch.distributed.get_rank() if num_pipeline_ranks > 1 else 0
+            
         with torch.device("meta"):
             model = Transformer(
                 model_args,
@@ -286,39 +245,10 @@ class Transformer(ModelBase, LoRALoaderMixin):
                 softmax_fp32=softmax_fp32,
             )
 
-        pt_model_file = Path(folder) / "consolidated.00.pth"
         safetensors_model_file = Path(folder) / "consolidated.safetensors"
+        assert safetensors_model_file.exists(), "safetensor file missing"
+        loaded = safetensors.torch.load_file(str(safetensors_model_file))
 
-        assert (
-            pt_model_file.exists() or safetensors_model_file.exists()
-        ), f"Make sure either {pt_model_file} or {safetensors_model_file} exists"
-        assert not (
-            pt_model_file.exists() and safetensors_model_file.exists()
-        ), f"Both {pt_model_file} and {safetensors_model_file} cannot exist"
-
-        if pt_model_file.exists():
-            loaded = torch.load(str(pt_model_file), mmap=True)
-        else:
-            loaded = safetensors.torch.load_file(str(safetensors_model_file))
-            
-        if quant_4bit:
-            quantized_loaded = {}
-            for name, tensor in loaded.items():
-                # Only quantize linear layer weights
-                if any(k in name for k in ["wq", "wk", "wv", "wo", "w1", "w2", "w3"]):
-                    # Quantize and add quantization state
-                    quant_tensor, quant_state = quantize_4bit(
-                        tensor.to(device=device, dtype=dtype),
-                        quant_type="nf4",
-                        compress_statistics=True
-                    )
-                    quantized_loaded[name] = quant_tensor
-                    quantized_loaded[f"{name}_quant_state"] = quant_state
-                else:
-                    # Copy other tensors as-is
-                    quantized_loaded[name] = tensor.to(device=device, dtype=dtype)
-        loaded = quantized_loaded
-
-        model.load_state_dict(loaded, assign=True, strict=False)
-
+        model.load_state_dict(loaded, assign=True, strict=True)
         return model.to(device=device, dtype=dtype)
+   
